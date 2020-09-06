ba3ef8078e5b5f625f733be349702f77057e9eed
kakarot96 <kunalaggarwal58@gmail.com>
2020-09-07 04:18:54 +0530

    account switched

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,5 @@
 export const sortData = (data:any)=>{
     const sortedData =  [...data];
     sortedData.sort((a:any,b:any)=>(a.cases>b.cases?-1:1))
-    return sortedData; 
+    return sortedData;
 }
