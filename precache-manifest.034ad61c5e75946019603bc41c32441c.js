a79e42b2035f588f5a28b1a700025e6324be11c4
kunal-compro <kunal.aggarwal@comprotechnologies.com>
2020-09-07 03:41:10 +0530

    Updates

---
+++ precache-manifest.034ad61c5e75946019603bc41c32441c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03fed75e1c7b9cc546333428fc46a987",
+    "url": "/covid-tracker/index.html"
+  },
+  {
+    "revision": "48114cb604f077b47021",
+    "url": "/covid-tracker/static/css/main.9c70d1a6.chunk.css"
+  },
+  {
+    "revision": "bb6412d223e2bf0ad7ef",
+    "url": "/covid-tracker/static/js/2.f22f6536.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/covid-tracker/static/js/2.f22f6536.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48114cb604f077b47021",
+    "url": "/covid-tracker/static/js/main.ed032768.chunk.js"
+  },
+  {
+    "revision": "a2c09be945803fa375fb",
+    "url": "/covid-tracker/static/js/runtime-main.948b6ba1.js"
+  }
+]);
